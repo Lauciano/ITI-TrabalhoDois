aea131bfaa86935139d1b7c27d5c8e09ec1bf89e
thiagovvb <thiagovvb@gmail.com>
2014-06-22 18:06:11 -0300

    Alfabeto

--- CodificadorPPM/src/CodificadorPPM.java
+++ CodificadorPPM/src/CodificadorPPM.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
+
 
 public class CodificadorPPM {
 	public static void main(String[] args){
 		System.out.println("************ Codificador PPM ***************");
 		
 		Leitor l = new Leitor("texto.txt");
+		ArrayList<Character> ar = l.getAlfabeto();
+		
+		for(int i = 0; i < ar.size(); i++){
+			System.out.println("Alfab [" + i + "] = " + ar.get(i));
+		}
+		
 		char c;
 		
 		while((c = l.getNextCharacter()) != '¬'){
 			System.out.println("lido = " + c + " and "  + (int)c);
 		}
 		
-		Contexto abc = new Contexto("S");
+		/*Contexto abc = new Contexto("S");
 		abc.addOcorrencia("S");
 		abc.addOcorrencia("S");
 		abc.addOcorrencia("B");
@@ -18,7 +26,7 @@ public class CodificadorPPM {
 		
 		for(int i = 0; i < abc.v.size(); i++){
 			System.out.println(abc.v.get(i).getSymbol() + " " + abc.v.get(i).getFrequencia() + " " + abc.v.get(i).getProbabilidade());
-		}
+		}*/
 		
 		/*Instancia a = abc.getInstancia("A");
 		System.out.println("Freq = " + a.getFrequencia() + " Prob = " + a.getProbabilidade());
--- CodificadorPPM/src/Leitor.java
+++ CodificadorPPM/src/Leitor.java
@@ -2,17 +2,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Leitor {
 
 	private BufferedReader br;
 	private String linhaAtual;
 	private int index;
+	private String nomeArq;
 	
 	public Leitor(String nomeArq){
 		
 		linhaAtual = "";
 		index = 0;
+		this.nomeArq = nomeArq;
 		
 		try{
 			br = new BufferedReader(new FileReader(nomeArq));
@@ -23,6 +26,32 @@ public class Leitor {
 		} catch (IOException e) {
 			System.err.println("Erro na leitura do arquivo!");
 		}
+	}
+	
+	public ArrayList<Character> getAlfabeto(){
+		String linha;
+		ArrayList<Character> alfabeto = new ArrayList<Character>();
+		
+		try {
+			
+			BufferedReader br2 = new BufferedReader(new FileReader(nomeArq));
+			for(int i = 0; (linha = br2.readLine()) != null; i++){
+				for(int j = 0; j < linha.length(); j++){
+					if(!alfabeto.contains(linha.charAt(j))){
+						alfabeto.add(linha.charAt(j));
+					}
+				}
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("Arquivo não encontrado!");
+		} catch (IOException e){
+			System.err.println("Erro na leitura do arquivo!");
+		}
+		
+		return alfabeto;
+ 		
 	}
 	
 	public char getNextCharacter(){
